db07a544282532874f6f2bad6fcf754a022f55fb
Rupal Jain <31166523+RupalJain97@users.noreply.github.com>
2018-10-11 22:17:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,29 @@
-var express=require('express');
 
 
-var app=express();
-var server=require('http').createServer(app);
+var express = require('express'), 
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+
+var port = 3000;
 
 
-var port=3000;
-
-server.listen(port,function(){
-    console.log("server"+port);
-    
-});
-
-var io=require('socket.io').listen(server);
-
-app.get("/",function(req,res){
-    res.sendFile(__dirname + '/index.html');
-});
-
-//to recieve events on server side
-io.sockets.on("connection",function(socket){   //use to make connection with socket
-   // console.log("socket connected");
-
-    socket.on("chat",function(data){
-        
-        io.sockets.emit('chat',data);
+io.sockets.on('connection', function(socket){
+    console.log("Socket created :" + socket.id)
+    socket.on('chat', function(data){
+        io.sockets.emit('chat', data);
         
     });
 
 });
 
+app.get("/",function(req,res){
+    console.log("entered app.get");
+    res.sendFile(__dirname + '/index.html');
+});
 
 
+server.listen(port,function(){
+    console.log("Listening server at port " + port);
+    
+});
